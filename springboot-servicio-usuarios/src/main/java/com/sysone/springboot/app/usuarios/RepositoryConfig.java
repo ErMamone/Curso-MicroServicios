352d9289f3938d7525ced25bd8acaac8ed2cca37
Eros <emamone@sysone.com>
2020-06-19 16:46:04 -0300

    seccion 7 - video 68

--- springboot-servicio-usuarios/src/main/java/com/sysone/springboot/app/usuarios/RepositoryConfig.java
+++ springboot-servicio-usuarios/src/main/java/com/sysone/springboot/app/usuarios/RepositoryConfig.java
@@ -4,8 +4,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
-import com.sysone.springboot.app.usuarios.models.entity.Rol;
-import com.sysone.springboot.app.usuarios.models.entity.Usuario;
+import com.sysone.springboot.app.usuarios.commons.models.entity.Rol;
+import com.sysone.springboot.app.usuarios.commons.models.entity.Usuario;
 
 @Configuration
 public class RepositoryConfig implements RepositoryRestConfigurer {
